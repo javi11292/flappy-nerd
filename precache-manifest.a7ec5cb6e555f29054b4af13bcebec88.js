69b5d94650875d0ece89538b4b00727bd5e538c4
javi11292 <javi_1_12_92@hotmail.com>
2019-02-15 20:30:17 +0100

    Updates

---
+++ precache-manifest.a7ec5cb6e555f29054b4af13bcebec88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c1e8e4f6bc7a2c595b3",
+    "url": "/flappy-nerd/static/js/main.2c1e8e4f.chunk.js"
+  },
+  {
+    "revision": "0fceb09ff2dc9dda9ef7",
+    "url": "/flappy-nerd/static/js/1.0fceb09f.chunk.js"
+  },
+  {
+    "revision": "6570378e81e8a3176636",
+    "url": "/flappy-nerd/static/js/runtime~main.6570378e.js"
+  },
+  {
+    "revision": "bb86e387da5f8c7f80738fae0b0c2e66",
+    "url": "/flappy-nerd/index.html"
+  }
+];
